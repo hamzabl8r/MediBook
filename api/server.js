594e9693ce0ed2cf5bc69c172a024a6bcbee8a83
Hamza Beji <hamzabeji001@gmail.com>
2025-10-20 11:46:57 +0100

    Fix CORS origin URL by removing trailing slash

--- api/server.js
+++ api/server.js
@@ -19,7 +19,7 @@ connectDB()
 app.use(express.json())
 const corsOptions = {
   origin: [
-    'https://medibook-project.vercel.app/',
+    'https://medibook-project.vercel.app',
     'http://localhost:3000', 
     'http://localhost:5173'  
   ],

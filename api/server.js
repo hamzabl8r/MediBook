cc543c8ec0b768c96f72d8b4228b733d452bade9
hamzabl8r <hamzabeji001@gmail.com>
2025-10-20 12:02:49 +0100

    medibook

--- api/server.js
+++ api/server.js
@@ -17,20 +17,9 @@ const Contact = require('./routes/contactRoutes')
 
 connectDB() 
 app.use(express.json())
-const corsOptions = {
-  origin: [
-    'https://medibook-project.vercel.app',
-    'http://localhost:3000', 
-    'http://localhost:5173'  
-  ],
-  credentials: true,
-  optionsSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
+
 
 app.use(cors(corsOptions));
-
 app.options('*', cors(corsOptions));
 app.get('/', (req, res) => {
     res.send('Hello World!')

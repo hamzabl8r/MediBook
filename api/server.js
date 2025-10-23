2b2ba4a3a1184fb5156081f7774eed3be092a2b3
hamzabl8r <hamzabeji001@gmail.com>
2025-10-23 09:59:12 +0100

    medibook

--- api/server.js
+++ api/server.js
@@ -23,6 +23,16 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
 
+// ---------------------
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+// ---------------------
+
+
 
 
 app.get('/', (req, res) => {
